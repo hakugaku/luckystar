bf7dbcc5cfb49b86bcf75416fe6896abd221c04e
hakugaku <131091824+hakugaku@users.noreply.github.com>
2024-01-13 18:07:53 +0900

    Update star.js

--- star.js
+++ star.js
@@ -1,9 +1,92 @@
 function check(){
-  if (document.getElementById('answer77').value == '夢') {
+  if (document.getElementById('starname').value == '夢') {
     alert('あたり!');
-  } else if (document.getElementById('answer77').value == 'ゆめ') {
+  } 
+    else if (document.getElementById('starname').value == 'ゆめ') {
     alert('あたり!');
-  } else {    
-    alert('はずれ!');
+  }
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+   
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  }
+   
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+   
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+   
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  }     
+    
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+
+    else if (document.getElementById('starname').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('starname').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+
+    else if (document.getElementById('starname').value == 'ゆめ') {
+    alert('あたり!');
+  } 
+    else if (document.getElementById('starname').value == '魚座') {
+    alert('自立心が刺激される年！ ひとつずつ自分で考えて決断するのが吉'
+  } 
+      
+  else {    
+    alert('もう一度確認してください');
   }
 }
