9b8e4905251da4efdf76ba9d7bcb28de781ae68b
hakugaku <131091824+hakugaku@users.noreply.github.com>
2024-01-13 17:54:53 +0900

    Create star.js

---
+++ star.js
@@ -0,0 +1,9 @@
+function check(){
+  if (document.getElementById('answer77').value == '夢') {
+    alert('あたり!');
+  } else if (document.getElementById('answer77').value == 'ゆめ') {
+    alert('あたり!');
+  } else {    
+    alert('はずれ!');
+  }
+}
