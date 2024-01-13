19cadf244a0d78995d7582133dd3bb5a87ffa833
hakugaku <131091824+hakugaku@users.noreply.github.com>
2024-01-13 19:34:58 +0900

    Update star.js

--- star.js
+++ star.js
@@ -1,88 +1,85 @@
 function check(){
-  if (document.getElementById('starname').value == '夢') {
-    alert('あたり!');
-  } 
-    else if (document.getElementById('starname').value == 'ゆめ') {
-    alert('あたり!');
+　　if (document.getElementById('starname').value == 'おひつじ') {
+    alert('準備＆計画はしっかりと！ コツコツとがんばるのが◎');
   }
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '牡羊') {
+    alert('準備＆計画はしっかりと！ コツコツとがんばるのが◎');
   } 
    
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'おうし') {
+    alert('アクティブに行動すればチャンスが巡ってくる♩');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '牡牛') {
+    alert('アクティブに行動すればチャンスが巡ってくる♩');
   }
    
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'ふたご') {
+    alert('下半期から運気アップ！ 自分を信じてトライして');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '双子') {
+    alert('下半期から運気アップ！ 自分を信じてトライして');
   } 
    
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'かに') {
+    alert('交友関係が広がる1年！ 運命的な出会いに期待しよう♡');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '蟹') {
+    alert('交友関係が広がる1年！ 運命的な出会いに期待しよう♡');
   } 
     
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'しし') {
+    alert('主役は“自分”！ 積極的にチャレンジできると◎');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '獅子') {
+    alert('主役は“自分”！ 積極的にチャレンジできると◎');
   } 
    
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'おとめ') {
+    alert('脱・マンネリ！ やりたいことに挑戦する1年に');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '乙女') {
+    alert('脱・マンネリ！ やりたいことに挑戦する1年に');
   }     
     
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'てんびん') {
+    alert('変化の年♩ 人からのお誘いに幸運が隠れているかも!?');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '天秤') {
+    alert('変化の年♩ 人からのお誘いに幸運が隠れているかも!?');
   } 
     
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'さそり') {
+    alert('キーパーソンが現れる予感♡ 大切な人とじっくり関係を深めよう！');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
-  } 
-
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
-  } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '蠍') {
+    alert('キーパーソンが現れる予感♡ 大切な人とじっくり関係を深めよう！');
   } 
 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'いて') {
+    alert('下半期に出会いあり♡ 早めに自分磨きをはじめよう！');
   } 
-    else if (document.getElementById('answer77').value == 'ゆめ') {
-    alert('あたり!');
-  } 
-
-    else if (document.getElementById('starname').value == 'ゆめ') {
-    alert('あたり!');
-  } 
-    else if (document.getElementById('starname').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == '射手') {
+    alert('下半期に出会いあり♡ 早めに自分磨きをはじめよう！');
   } 
 
-    else if (document.getElementById('starname').value == 'ゆめ') {
-    alert('あたり!');
+    else if (document.getElementById('starname').value == 'やぎ') {
+    alert('大忙しの1年に！ 巡ってきたチャンスは逃さないで');
   } 
-    else if (document.getElementById('starname').value == '魚座') {
+    else if (document.getElementById('starname').value == '山羊') {
+    alert('大忙しの1年に！ 巡ってきたチャンスは逃さないで');
+  } 
+
+    else if (document.getElementById('starname').value == 'みずがめ') {
+    alert('心地よい暮らしを整えて、どんどんトライするのがGOOD！');
+  } 
+    else if (document.getElementById('starname').value == '水瓶') {
+    alert('心地よい暮らしを整えて、どんどんトライするのがGOOD！');
+  } 
+
+    else if (document.getElementById('starname').value == 'うお') {
+    alert('自立心が刺激される年！ ひとつずつ自分で考えて決断するのが吉');
+  } 
+    else if (document.getElementById('starname').value == '魚') {
     alert('自立心が刺激される年！ ひとつずつ自分で考えて決断するのが吉'
   } 
       
